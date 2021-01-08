b7865bfa2d8684c7c701a928c59bddad10845f07
wicker <bwicker@cedarville.edu>
2021-01-08 09:39:59 -0500

    sort recipes on fetch

--- src/services/recipes/RecipeController.ts
+++ src/services/recipes/RecipeController.ts
@@ -1,8 +1,14 @@
 import { getRepository } from "typeorm"
 import { Recipe } from "./Recipe"
 
+const compareNames = ( r1: Recipe, r2: Recipe ) => {
+    const R1 = r1.name.toUpperCase();
+    const R2 = r2.name.toUpperCase();
+    return (R1 > R2) ? 1 : ((R2 > R1) ? -1 : 0)
+}
+
 export const getRecipes = async () => {
-    const recipes = await getRepository(Recipe).find({ relations: ["ingredients"]})
+    const recipes = await (await getRepository(Recipe).find({ relations: ["ingredients"]})).sort(compareNames)
     return recipes;
 }
 
