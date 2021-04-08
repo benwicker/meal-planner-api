38990c2b8d1d144f701c7fc6dd902c29f48f898c
wicker <bwicker@cedarville.edu>
2021-04-08 11:31:52 -0400

    another fix attempt

--- src/services/misc/MiscController.ts
+++ src/services/misc/MiscController.ts
@@ -1,8 +1,10 @@
 import { getRepository } from "typeorm"
 import { Recipe } from "../recipes/Recipe"
+import { initializeDbConnection } from '../../database';
 
 export const checkConnection = async () => {
   try {
+    initializeDbConnection();
     const recipes = await getRepository(Recipe).find({ take: 1 });
     return recipes;
   } catch (e) {
